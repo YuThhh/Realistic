f8f5b5708e2fa5f61ce6ca34544c034764c641ba
YuThhh <101546506+YuThhh@users.noreply.github.com>
2026-05-05 22:11:37 +0900

    피해 이벤트

--- src/main/java/org/role/realistic/Damage.java
+++ src/main/java/org/role/realistic/Damage.java
@@ -1,5 +1,7 @@
 package org.role.realistic;
 
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.damage.DamageSource;
 import org.bukkit.damage.DamageType;
@@ -9,10 +11,32 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.Map;
 import java.util.Objects;
+import java.util.UUID;
 
-public class Damage implements Listener {
+public class Damage extends Util implements Listener {
+
+    public Damage(Realistic real, Values values) {
+        super(real, values);
+        bleed();
+    }
+
+    public void bleed() {
+        new BukkitRunnable() {
+            public void run() {
+                for (Player p : Bukkit.getOnlinePlayers()) {
+                    UUID uuid = p.getUniqueId();
+                    Map<String, Tag> tags = values.getTags(uuid);
+                    if (tags.containsKey("bleed")) {
+                        p.damage(Math.random()*1.7);
+                    }
+                }
+            }
+        }.runTaskTimer(real, 0, 4);
+    }
 
     @EventHandler
     public void onDamage(EntityDamageEvent event) {
@@ -20,13 +44,36 @@ public class Damage implements Listener {
             return;
         }
 
-        double hp = Objects.requireNonNull(Objects.requireNonNull(p).getAttribute(Attribute.MAX_HEALTH)).getValue();
-        if (event.getDamage() > hp / 4.0) {
-            p.addPotionEffect(new PotionEffect(PotionEffectType.SLOWNESS, 40, 3));
+        if (event.getCause() == EntityDamageEvent.DamageCause.CUSTOM) {
+            return;
         }
 
-        if (event.getDamageSource().getDamageType() == DamageType.EXPLOSION) {
-            p.addPotionEffect(new PotionEffect(PotionEffectType.NAUSEA, 250, 5));
+        DamageType damageCause = event.getDamageSource().getDamageType();
+        UUID uuid = p.getUniqueId();
+        double hp = Objects.requireNonNull(p.getAttribute(Attribute.MAX_HEALTH)).getValue();
+        if (event.getFinalDamage() > hp / 4.0) {
+            addTag(uuid, "shock", "쇼크", NamedTextColor.YELLOW, 100);
+            addPotionEffect(p, PotionEffectType.SLOWNESS, 100, 3);
+            addPotionEffect(p, PotionEffectType.BLINDNESS, 100, 1);
+        }
+
+        if (damageCause == DamageType.ARROW || damageCause == DamageType.TRIDENT) {
+            if (Math.random() < 0.33) { // 더 직관적인 확률 계산
+                addTag(uuid, "bleed", "출혈", NamedTextColor.RED, 70);
+            }
+        }
+
+        if (damageCause == DamageType.EXPLOSION || damageCause == DamageType.PLAYER_EXPLOSION) {
+            if (event.getFinalDamage() > hp / 2.0) {
+                addTag(uuid, "weak_concussion", "약한 뇌진탕", NamedTextColor.YELLOW, 100);
+                addPotionEffect(p, PotionEffectType.NAUSEA, 100, 2);
+            } else if (event.getFinalDamage() > hp / 6.0) {
+                addTag(uuid, "concussion", "뇌진탕", NamedTextColor.YELLOW, 250);
+                addPotionEffect(p, PotionEffectType.NAUSEA, 250, 3);
+            } else if (event.getFinalDamage() > hp / 10.0) {
+                addTag(uuid, "insane_concussion", "심각한 뇌진탕", NamedTextColor.YELLOW, 500);
+                addPotionEffect(p, PotionEffectType.NAUSEA, 500, 5);
+            }
         }
     }
 }
