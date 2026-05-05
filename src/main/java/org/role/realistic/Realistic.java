527b64cca9f799dcf2ef96e452f87d12d1638f3d
YuThhh <101546506+YuThhh@users.noreply.github.com>
2026-05-05 22:12:49 +0900

    메인

--- src/main/java/org/role/realistic/Realistic.java
+++ src/main/java/org/role/realistic/Realistic.java
@@ -1,13 +1,69 @@
 package org.role.realistic;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.*;
 
 public final class Realistic extends JavaPlugin {
+    Values value = new Values();
+    Thirsty thirsty = new Thirsty(this, value);
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getServer().getPluginManager().registerEvents(new Damage(), this);
+        getServer().getPluginManager().registerEvents(new Damage(this, value), this);
+        getServer().getPluginManager().registerEvents(new Food(this, value), this);
+        Objects.requireNonNull(getCommand("thirsty")).setExecutor(new Cmd(this, value));
+        Objects.requireNonNull(getCommand("bleed")).setExecutor(new Cmd(this, value));
+        thirsty.startThirsty();
+
+        startActionBar();
+    }
+
+    public void startActionBar() {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (Player p : Bukkit.getOnlinePlayers()) {
+                    UUID uuid = p.getUniqueId();
+                    int currentThirsty = value.getThirsty(uuid);
+                    Map<String, Tag> tags = value.getTags(uuid);
+
+                    List<String> toRemove = new ArrayList<>();
+
+                    for (Map.Entry<String, Tag> entry : tags.entrySet()) {
+                        entry.getValue().decreaseDuration(1); // 1초(20틱)씩 감소[cite: 6]
+                        if (entry.getValue().getDuration() <= 0) {
+                            toRemove.add(entry.getKey());
+                        }
+                    }
+
+                    // 만료된 태그 제거
+                    for (String tagName : toRemove) {
+                        value.removeTag(uuid, tagName);
+                    }
+
+                    Component tagComponent = Component.empty();
+
+                    for (Map.Entry<String, Tag> entry : tags.entrySet()) {
+                        Component eachTag = Component.text(" [" +entry.getValue().getName()+"]", entry.getValue().getColor());
+
+                        tagComponent = tagComponent.append(eachTag);
+                    }
+
+                    Component message = Component.text("갈증: " + currentThirsty, NamedTextColor.BLUE)
+                            .append(tagComponent);
+
+                    p.sendActionBar(message);
+
+                }
+            }
+        }.runTaskTimer(this, 0L,1L);
     }
 
     @Override
