c773db631bd61c208e9b6bb7ef4d62ada42bc04e
YuThhh <101546506+YuThhh@users.noreply.github.com>
2026-05-05 22:11:51 +0900

    소비 이벤트

---
+++ src/main/java/org/role/realistic/Food.java
@@ -0,0 +1,35 @@
+package org.role.realistic;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+
+import java.util.UUID;
+
+public class Food extends Util implements Listener {
+
+    public Food(Realistic real, Values values) {
+        super(real, values);
+    }
+
+    @EventHandler
+    public void onEat(PlayerItemConsumeEvent e) {
+        Player p = e.getPlayer();
+        UUID uuid = p.getUniqueId();
+        Material item = e.getItem().getType();
+        int currentThirsty = getThirsty(uuid);
+
+        if (item == Material.POTION || item == Material.MILK_BUCKET) {
+            setThirsty(uuid, currentThirsty + 20);
+        } else if (item == Material.APPLE) {
+            setThirsty(uuid, currentThirsty + 7);
+        } else if (item == Material.MELON_SLICE) {
+            setThirsty(uuid, currentThirsty + 10);
+        } else if (item == Material.SWEET_BERRIES || item == Material.GLOW_BERRIES) {
+            setThirsty(uuid, currentThirsty + 3);
+        }
+    }
+}
