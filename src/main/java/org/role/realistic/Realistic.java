37c2cd5ded907401251d4fd0182f5a37e8fd571e
최우준 <choiwj345300@gmail.com>
2026-05-07 12:55:56 +0900

    태그 매니저 추가

--- src/main/java/org/role/realistic/Realistic.java
+++ src/main/java/org/role/realistic/Realistic.java
@@ -5,8 +5,6 @@ import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.*;
@@ -23,11 +21,11 @@ public final class Realistic extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new Food(this, value), this);
         getServer().getPluginManager().registerEvents(new Inventory(this, value), this);
         getServer().getPluginManager().registerEvents(new Stamina(this, value), this);
+        getServer().getPluginManager().registerEvents(new TagManager(this, value), this);
         Objects.requireNonNull(getCommand("thirsty")).setExecutor(new Cmd(this, value));
         Objects.requireNonNull(getCommand("bleed")).setExecutor(new Cmd(this, value));
         thirsty.startThirsty();
 
-        startCheckTag();
         startActionBar();
     }
 
@@ -72,51 +70,6 @@ public final class Realistic extends JavaPlugin {
                 }
             }
         }.runTaskTimer(this, 0L,1L);
-    }
-
-    public void startCheckTag() {
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                for (Player p : Bukkit.getOnlinePlayers()) {
-                    UUID uuid = p.getUniqueId();
-                    Map<String, Tag> tags = value.getTags(uuid);
-
-                    if (tags.containsKey("thirsty")) {
-                        util.addPotionEffect(p, PotionEffectType.SLOWNESS, tags.get("thirsty").getDuration(), tags.get("thirsty").getAmplifier());
-                    } else if (tags.containsKey("insane_thirsty")) {
-                        util.addPotionEffect(p, PotionEffectType.SLOWNESS, tags.get("insane_thirsty").getDuration(), tags.get("insane_thirsty").getAmplifier());
-                        util.addPotionEffect(p, PotionEffectType.MINING_FATIGUE, tags.get("insane_thirsty").getDuration(), tags.get("insane_thirsty").getAmplifier());
-                    } else if (tags.containsKey("deadly_thirsty")) {
-                        util.addPotionEffect(p, PotionEffectType.SLOWNESS, tags.get("deadly_thirsty").getDuration(), tags.get("deadly_thirsty").getAmplifier());
-                        util.addPotionEffect(p, PotionEffectType.MINING_FATIGUE, tags.get("deadly_thirsty").getDuration(), tags.get("deadly_thirsty").getAmplifier()-1);
-                    }
-
-                    if (tags.containsKey("exhaust")) {
-                        util.addPotionEffect(p, PotionEffectType.SLOWNESS, tags.get("exhaust").getDuration(), tags.get("exhaust").getAmplifier());
-                    }
-
-                    if (tags.containsKey("broken")) {
-                        util.addPotionEffect(p, PotionEffectType.SLOWNESS, tags.get("broken").getDuration(), tags.get("broken").getAmplifier());
-                    } else if (tags.containsKey("insane_broken")) {
-                        util.addPotionEffect(p, PotionEffectType.SLOWNESS, tags.get("insane_broken").getDuration(), tags.get("insane_broken").getAmplifier());
-                    }
-
-                    if (tags.containsKey("weak_concussion")) {
-                        util.addPotionEffect(p, PotionEffectType.NAUSEA, tags.get("weak_concussion").getDuration(), tags.get("weak_concussion").getAmplifier());
-                    } else if (tags.containsKey("concussion")) {
-                        util.addPotionEffect(p, PotionEffectType.NAUSEA, tags.get("concussion").getDuration(), tags.get("concussion").getAmplifier());
-                    } else if (tags.containsKey("insane_concussion")) {
-                        util.addPotionEffect(p, PotionEffectType.NAUSEA, tags.get("insane_concussion").getDuration(), tags.get("insane_concussion").getAmplifier());
-                    }
-
-                    if (tags.containsKey("shock")) {
-                        util.addPotionEffect(p, PotionEffectType.SLOWNESS, tags.get("shock").getDuration(), tags.get("shock").getAmplifier());
-                        util.addPotionEffect(p, PotionEffectType.BLINDNESS, tags.get("shock").getDuration(), tags.get("shock").getAmplifier()-1);
-                    }
-                }
-            }
-        }.runTaskTimer(this,0L,1L);
     }
 
     @Override
---
+++ src/main/java/org/role/realistic/TagManager.java
@@ -0,0 +1,63 @@
+package org.role.realistic;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class TagManager extends Util implements Listener {
+    public TagManager(Realistic real, Values values) {
+        super(real, values);
+
+        startCheckTag();
+    }
+
+    public void startCheckTag() {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (Player p : Bukkit.getOnlinePlayers()) {
+                    UUID uuid = p.getUniqueId();
+                    Map<String, Tag> tags = getTags(uuid);
+
+                    if (tags.containsKey("thirsty")) {
+                        addPotionEffect(p, PotionEffectType.SLOWNESS, tags.get("thirsty").getDuration(), tags.get("thirsty").getAmplifier());
+                    } else if (tags.containsKey("insane_thirsty")) {
+                        addPotionEffect(p, PotionEffectType.SLOWNESS, tags.get("insane_thirsty").getDuration(), tags.get("insane_thirsty").getAmplifier());
+                        addPotionEffect(p, PotionEffectType.MINING_FATIGUE, tags.get("insane_thirsty").getDuration(), tags.get("insane_thirsty").getAmplifier());
+                    } else if (tags.containsKey("deadly_thirsty")) {
+                        addPotionEffect(p, PotionEffectType.SLOWNESS, tags.get("deadly_thirsty").getDuration(), tags.get("deadly_thirsty").getAmplifier());
+                        addPotionEffect(p, PotionEffectType.MINING_FATIGUE, tags.get("deadly_thirsty").getDuration(), tags.get("deadly_thirsty").getAmplifier()-1);
+                    }
+
+                    if (tags.containsKey("exhaust")) {
+                        addPotionEffect(p, PotionEffectType.SLOWNESS, tags.get("exhaust").getDuration(), tags.get("exhaust").getAmplifier());
+                    }
+
+                    if (tags.containsKey("broken")) {
+                        addPotionEffect(p, PotionEffectType.SLOWNESS, tags.get("broken").getDuration(), tags.get("broken").getAmplifier());
+                    } else if (tags.containsKey("insane_broken")) {
+                        addPotionEffect(p, PotionEffectType.SLOWNESS, tags.get("insane_broken").getDuration(), tags.get("insane_broken").getAmplifier());
+                    }
+
+                    if (tags.containsKey("weak_concussion")) {
+                        addPotionEffect(p, PotionEffectType.NAUSEA, tags.get("weak_concussion").getDuration(), tags.get("weak_concussion").getAmplifier());
+                    } else if (tags.containsKey("concussion")) {
+                        addPotionEffect(p, PotionEffectType.NAUSEA, tags.get("concussion").getDuration(), tags.get("concussion").getAmplifier());
+                    } else if (tags.containsKey("insane_concussion")) {
+                        addPotionEffect(p, PotionEffectType.NAUSEA, tags.get("insane_concussion").getDuration(), tags.get("insane_concussion").getAmplifier());
+                    }
+
+                    if (tags.containsKey("shock")) {
+                        addPotionEffect(p, PotionEffectType.SLOWNESS, tags.get("shock").getDuration(), tags.get("shock").getAmplifier());
+                        addPotionEffect(p, PotionEffectType.BLINDNESS, tags.get("shock").getDuration(), tags.get("shock").getAmplifier()-1);
+                    }
+                }
+            }
+        }.runTaskTimer(real,0L,1L);
+    }
+}
